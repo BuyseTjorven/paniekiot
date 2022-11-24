19b4f5fd96e924dd1321dbaeaffeb5c3753db2f1
BuyseTjorven <tjorven.buyse@student.howest.be>
2022-11-24 11:40:19 +0100

    ergens op einde doc kon niemeer volgen send message doe raar idk bro twerkt allemaal half xx

---
+++ device/Models/DeviceConfiguration.cs
@@ -0,0 +1,8 @@
+namespace device.Models;
+
+public class DeviceConfigInfo
+{
+    public int threshold { get; set; }
+
+    public int maxrpm { get; set; }
+}
--- device/Program.cs
+++ device/Program.cs
@@ -2,7 +2,9 @@
 using Microsoft.Azure.Devices.Client;
 using device.Models;
 using Newtonsoft.Json;
+using Microsoft.Azure.Devices.Shared;
 
+DeviceConfigInfo configInfo = new DeviceConfigInfo();
 var connectionString = "HostName=tjorvenIOT.azure-devices.net;DeviceId=pctjorven;SharedAccessKey=vhLhIx3dmdN7ptyglg87Qks62ZD6S6m+QYIUQhCZ4wM=";
 
 using var deviceClient = DeviceClient.CreateFromConnectionString(connectionString);
@@ -11,7 +13,18 @@ using var deviceClient = DeviceClient.CreateFromConnectionString(connectionStrin
 //// open connection explicitly
 await deviceClient.OpenAsync();
 await deviceClient.SetReceiveMessageHandlerAsync(ReceiveMessage, null);
+await deviceClient.SetDesiredPropertyUpdateCallbackAsync(OnDesiredPropertyChanged, null);
+await ForceDeviceTwinRetrieval();
 
+#region Boot
+var reportedProperties = new TwinCollection
+{
+    ["BootTime"] = DateTime.Now
+};
+
+await deviceClient.UpdateReportedPropertiesAsync(reportedProperties);
+
+#endregion
 
 while (true)
 {
@@ -19,6 +32,21 @@ while (true)
     Thread.Sleep(1000);
 }
 
+async Task OnDesiredPropertyChanged(TwinCollection desiredProperties, object userContext)
+{
+    Console.WriteLine("One or more device twin desired properties changed:");
+    Console.WriteLine(JsonConvert.SerializeObject(desiredProperties));
+    configInfo = JsonConvert.DeserializeObject<DeviceConfigInfo>(desiredProperties.ToJson());
+
+}
+
+async Task ForceDeviceTwinRetrieval()
+{
+    var twin = await deviceClient.GetTwinAsync();
+    Console.WriteLine(twin.Properties.Desired.ToJson());
+    configInfo = JsonConvert.DeserializeObject<DeviceConfigInfo>(twin.Properties.Desired.ToJson());
+    Console.WriteLine("The Devicetwin is forced retrieved");
+}
 async Task ReceiveMessage(Message message, object userContext)
 {
     var messageData = Encoding.ASCII.GetString(message.GetBytes());
@@ -31,10 +59,13 @@ async Task SendMessage()
     TemperatureMessage tpm = new TemperatureMessage();
     Random rnd = new Random();
     tpm.Temperature = rnd.Next(0, 100);
-
-    var json = JsonConvert.SerializeObject(tpm);
-    using var message = new Message(Encoding.UTF8.GetBytes(json));
-    await deviceClient.SendEventAsync(message);
-    Console.WriteLine("A single message is sent");
+    if (tpm.Temperature > configInfo.threshold)
+    {
+        var json = JsonConvert.SerializeObject(tpm);
+        using var message = new Message(Encoding.UTF8.GetBytes(json));
+        Console.WriteLine("Message sent: {0}", message);
+        Console.WriteLine("A single message is sent");
+        await deviceClient.SendEventAsync(message);
+    }
 
 }
